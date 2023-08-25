441b2ba5ff225638bb273f40c36cc9dcd1a1dedd
tiago <tgribeiro@gmail.com>
2023-08-25 10:41:23 -0300

    Novo teste

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -159,12 +159,6 @@ public class ContaDAO {
         }
     }
 
-
-    public void teste() {
-        System.out.println("teste");
-    }
-
-
     public void alterarLogico(Integer numeroDaConta){
         PreparedStatement ps;
         String sql = "UPDATE conta SET esta_ativa = false WHERE numero = ?";
